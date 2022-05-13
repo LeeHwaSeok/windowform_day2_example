6c104e1f69e503393c6fcb879f794804317f0bb3
20171062@student.anu.ac.kr <20171062@student.anu.ac.kr>
2022-05-13 14:10:20 +0900

    초기 커밋.

---
+++ windowform_day2/Form1.Designer.cs
@@ -0,0 +1,283 @@
+﻿namespace windowform_day2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.TreeNode treeNode2 = new System.Windows.Forms.TreeNode("동물");
+            this.tb_name = new System.Windows.Forms.TextBox();
+            this.tb_phone = new System.Windows.Forms.TextBox();
+            this.tb_group = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.b_add = new System.Windows.Forms.Button();
+            this.b_remove = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.list_phone = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.list_group = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.list_name1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tb_name
+            // 
+            this.tb_name.Font = new System.Drawing.Font("굴림", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.tb_name.Location = new System.Drawing.Point(190, 36);
+            this.tb_name.Name = "tb_name";
+            this.tb_name.Size = new System.Drawing.Size(339, 40);
+            this.tb_name.TabIndex = 0;
+            this.tb_name.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // tb_phone
+            // 
+            this.tb_phone.Font = new System.Drawing.Font("굴림", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.tb_phone.Location = new System.Drawing.Point(190, 90);
+            this.tb_phone.Name = "tb_phone";
+            this.tb_phone.Size = new System.Drawing.Size(339, 40);
+            this.tb_phone.TabIndex = 1;
+            // 
+            // tb_group
+            // 
+            this.tb_group.Font = new System.Drawing.Font("굴림", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.tb_group.Location = new System.Drawing.Point(189, 148);
+            this.tb_group.Name = "tb_group";
+            this.tb_group.Size = new System.Drawing.Size(340, 40);
+            this.tb_group.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("나눔스퀘어OTF ExtraBold", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(61, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 31);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "이   름 :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("나눔스퀘어OTF ExtraBold", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label2.Location = new System.Drawing.Point(57, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 31);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "휴대폰 :";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("나눔스퀘어OTF ExtraBold", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label3.Location = new System.Drawing.Point(61, 142);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(103, 31);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "소   속 :";
+            // 
+            // b_add
+            // 
+            this.b_add.Font = new System.Drawing.Font("나눔스퀘어OTF", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.b_add.Location = new System.Drawing.Point(570, 36);
+            this.b_add.Name = "b_add";
+            this.b_add.Size = new System.Drawing.Size(135, 68);
+            this.b_add.TabIndex = 6;
+            this.b_add.Text = "추가";
+            this.b_add.UseVisualStyleBackColor = true;
+            this.b_add.Click += new System.EventHandler(this.b_add_Click);
+            // 
+            // b_remove
+            // 
+            this.b_remove.Font = new System.Drawing.Font("나눔스퀘어OTF", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.b_remove.Location = new System.Drawing.Point(570, 120);
+            this.b_remove.Name = "b_remove";
+            this.b_remove.Size = new System.Drawing.Size(135, 68);
+            this.b_remove.TabIndex = 7;
+            this.b_remove.Text = "삭제";
+            this.b_remove.UseVisualStyleBackColor = true;
+            this.b_remove.Click += new System.EventHandler(this.b_remove_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.list_name1,
+            this.list_phone,
+            this.list_group});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(44, 245);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(661, 237);
+            this.listView1.TabIndex = 8;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // list_phone
+            // 
+            this.list_phone.Text = "휴대폰 번호";
+            this.list_phone.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.list_phone.Width = 150;
+            // 
+            // list_group
+            // 
+            this.list_group.Text = "소속";
+            this.list_group.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.list_group.Width = 150;
+            // 
+            // list_name1
+            // 
+            this.list_name1.Text = "이름";
+            this.list_name1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.list_name1.Width = 150;
+            // 
+            // treeView1
+            // 
+            this.treeView1.CheckBoxes = true;
+            this.treeView1.Location = new System.Drawing.Point(44, 522);
+            this.treeView1.Name = "treeView1";
+            treeNode2.Name = "root";
+            treeNode2.Text = "동물";
+            this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode2});
+            this.treeView1.Size = new System.Drawing.Size(661, 481);
+            this.treeView1.TabIndex = 9;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(783, 522);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(464, 28);
+            this.textBox1.TabIndex = 10;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(783, 571);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(464, 56);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(783, 642);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(464, 56);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(783, 723);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(464, 56);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(783, 811);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(464, 56);
+            this.button4.TabIndex = 14;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(783, 897);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(464, 56);
+            this.button5.TabIndex = 15;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1623, 1189);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.b_remove);
+            this.Controls.Add(this.b_add);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tb_group);
+            this.Controls.Add(this.tb_phone);
+            this.Controls.Add(this.tb_name);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tb_name;
+        private System.Windows.Forms.TextBox tb_phone;
+        private System.Windows.Forms.TextBox tb_group;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button b_add;
+        private System.Windows.Forms.Button b_remove;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader list_phone;
+        private System.Windows.Forms.ColumnHeader list_group;
+        private System.Windows.Forms.ColumnHeader list_name1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ windowform_day2/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace windowform_day2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void b_add_Click(object sender, EventArgs e)
+        {
+            //공백 예외처리
+            if (tb_group.Text == "" || tb_name.Text == "" || tb_phone.Text == "")
+            {
+                MessageBox.Show("내용을 입력하세요");
+            }
+            else
+            { //데이터 추가형식 '배열'
+                string[] slist = new string[] { tb_name.Text, tb_phone.Text, tb_group.Text };
+                ListViewItem viewItem = new ListViewItem(slist);
+                listView1.Items.Add(viewItem);
+
+                //텍스트 초기화
+                tb_phone.Clear();
+                tb_group.Clear();
+                tb_name.Clear();
+
+            }
+        }
+
+        private void b_remove_Click(object sender, EventArgs e)
+        {
+            //아이템이 비어있을경우
+            if (listView1.Items.Count != 0)
+            {
+                //              listView1.Items.RemoveAt(listView1.FocusedItem.Index);    1번 사용 가능
+                listView1.FocusedItem.Remove();                          //2번 사용가능
+            }
+            else
+            {
+                MessageBox.Show("지울게 없습니다.");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TreeNode node = new TreeNode(textBox1.Text);
+
+            /*            if(treeView1.SelectedNode != null && treeView1.SelectedNode.IsSelected)
+                        {
+                            treeView1.SelectedNode.Nodes.Add(node); 
+                        }
+                        else
+                        {
+                            treeView1.Nodes.Add(node);
+                        }*/
+            //체크 + 선택이 되야만 추가 됌.
+            if (treeView1.SelectedNode.Checked)
+            {
+                treeView1.SelectedNode.Nodes.Add(node);
+            }
+            else
+            {
+                treeView1.Nodes.Add(node);
+            }
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            treeView1.SelectedNode.Remove();
+        }
+
+
+        List<TreeNode> chknode = new List<TreeNode>();
+        void removechk(TreeNodeCollection nodes)
+        {
+            foreach (TreeNode node in nodes)
+            {
+                if (node.Checked)
+                {
+                    chknode.Add(node);
+                }
+                else
+                {
+                    removechk(node.Nodes);
+                }
+            }
+            foreach (TreeNode chkn in chknode)
+            {
+                nodes.Remove(chkn);
+            }
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            removechk(treeView1.Nodes);
+        }
+    }
+}
